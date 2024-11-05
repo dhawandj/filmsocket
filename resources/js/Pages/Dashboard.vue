b9ef7c2c676e6ca595e2cfef92d2a48f3d84bdc5
dhawandj <dhawanreddy9900@gmail.com>
2024-11-05 11:04:27 +0530

    first commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,43 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
+            >
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl space-y-3 sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
+                >
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        You're logged in!
+                    </div>
+                </div>
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
+                >
+                    <div
+                        class="p-6 font-bold capitalize text-gray-900 dark:text-gray-100"
+                    >
+                        <span class="text-red-500"> filmsocket </span>
+                        <span class="text-yellow-400">
+                            connect all the film makers reddy
+                        </span>
+                        <p>Comming soon</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
