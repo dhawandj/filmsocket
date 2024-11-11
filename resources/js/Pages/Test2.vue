59c16cb69d1a3db0c595dbc0872d0c9be5703370
dhawandj <dhawanreddy9900@gmail.com>
2024-11-11 19:29:03 +0530

    checking ui in large device 10

--- resources/js/Pages/Test2.vue
+++ resources/js/Pages/Test2.vue
@@ -4,13 +4,14 @@ import imgUrl from './../../images/camera2.png';
 </script>
 <template>
     <main class="h-svh borde border-red-500 space-y-3 overflow-y-scroll ">
-        <nav class=" sticky top-0 flex justify-between  bg-secondaryAlt text-textPrimary z-50 text mx1 mt-2 rounded-sm borde border-secondary shadow-2xl  p-3 text-center ">
+        <nav class=" sticky top-0 flex justify-between  bg-secondaryAlt text-textPrimary z-50 text mx1 mt- rounded-sm borde border-secondary shadow-2xl  p-3 text-center ">
           <span>filmsocket.com</span>  
           <span class="text-sm">◉❖</span>
         </nav>
         <section class="borde border-secondary relative  bg-[#090C11]  mx-2 rounded-sm min-h-[30%] overflow-hidden">
-                <img class="h-[100%] w-full object-contain absolute bottom-0 -right-2 z-0  object-right   borde "   :src="imgUrl" alt="">
-                <button class="borde absolute bottom-5 left-5 text-textPrimary rounded-md   text-xs p-2 bg-secondaryAlt">what you want</button>
+                <img class="h-[130%] w-full object-contain absolute bottom-0 -right-7 z-0  object-right   borde "   :src="imgUrl" alt="">
+                <button class="borde absolute bottom-5 left-2 text-textPrimary rounded-md   text-xs p-2 bg-secondaryAlt">what you want</button>
+                <button class="borde absolute bottom-5 left-32   text-textPrimary rounded-md   text-xs p-2 bg-secondaryAlt">what you have</button>
         </section>
         <nav class="text mx-2 mt-2 rounded-sm border border-secondary p-1 text-center text-sm">
             filmsocket.com
