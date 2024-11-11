c3cca5ea39e046cd0966eb4f60b126b0f0cc73c4
dhawandj <dhawanreddy9900@gmail.com>
2024-11-11 18:43:37 +0530

    ui test for mobile 6

--- resources/js/Pages/Test2.vue
+++ resources/js/Pages/Test2.vue
@@ -4,7 +4,7 @@ import imgUrl from './../../images/camera2.png';
 </script>
 <template>
     <main class="h-svh borde border-red-500 space-y-3 ">
-        <nav class="text mx-2 mt-2 rounded-sm border border-secondary p-2 text-center text-xs">
+        <nav class=" sticky top-2 bg-background z-50 text mx-2 mt-2 rounded-sm border border-secondary p-2 text-center text-xs">
             filmsocket.com
         </nav>
         <section class="border border-secondary relative  bg-[#090C11]  mx-2 rounded-sm min-h-[35%] overflow-hidden">
