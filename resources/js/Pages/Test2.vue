102ad26806a76fa1fea0066278dea4ece2d3f8f5
dhawandj <dhawanreddy9900@gmail.com>
2024-11-13 10:18:40 +0530

    new ui 1.0

--- resources/js/Pages/Test2.vue
+++ resources/js/Pages/Test2.vue
@@ -5,42 +5,64 @@ import imgUrl from './../../images/camera2.png';
     <main class="min-h-svh borderr p-2">
         <nav class="nav borderr borderr-red-400">filmsocket.com</nav>
         <section class="borderr-4 borderr-red-400">
-            <div class="borderr-2 min-h-[30vh] max-h-[30vh] bg-zinc-900 rounded-md   relative overflow-hidden ">
-                <img class="h-[40vh]  -right-4  bottom-0   absolute    borderr-green-400 borderr"  :src="imgUrl" alt="">
+            <div class="borderr-2 min-h-[25vh] max-h-[25vh] bg-zinc-900 rounded-md   relative overflow-hidden ">
+                <img class="h-[30vh]  -right-4  bottom-0   absolute    borderr-green-400 borderr"  :src="imgUrl" alt="">
                 <!-- <button class="borderr px-2 py-1 text-sm   absolute bottom-[2vh] left-[2vh] rounded-md ">what you what</button> -->
                 <!-- <button class="borderr px-2 py-1 text-sm  absolute bottom-[2vh] left-[30vh] rounded-md ">what you what</button> -->
             </div>
         </section>
         <section class="borderr-4 borderr-red-400 space-y-3">
-            <p class="text-textPrimary">Connect..</p>
-            <div class="p-1 flex justify-around gap-x-3">
-                <button class="borde   px-3 flex-1  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">what you what</button>
-                <button class="borde px-3 py-2 flex-1  text-secondary text-sm bg-secondaryAlt  rounded-md ">what you what</button>
-            </div>
-            <p class="text-textPrimary">filmMaking</p>
-            <div class="p-1 flex justify-around gap-x-3">
-                <button class="borde px-3 flex-1  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">pre-production</button>
-                <button class="borde px-3 py-2 flex-1  text-secondary text-sm bg-secondaryAlt  rounded-md ">post-production</button>
-            </div>
-            <p class="text-textPrimary">Markting</p>
-            <div class="p-1 grid  grid-cols-3 gap-2   ">
-                <button class="borde px-3 flex-1  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">Promations</button>
-                <button class="borde px-3 py-2 flex-1  text-secondary text-sm bg-secondaryAlt  rounded-md ">Sposnors</button>
-                <button class="borde px-3 py-2 flex-1  text-secondary text-sm bg-secondaryAlt  rounded-md ">Money</button>
-                <button class="borde px-3 py-2 flex-1  text-secondary text-sm bg-secondaryAlt  rounded-md ">Honey</button>
-                <button class="borde px-3 py-2 flex-1 col-span-2  text-secondary text-sm bg-secondaryAlt  rounded-md ">Honey</button>
-                <button class="borde px-3 py-2 flex-1 col-span-2  text-secondary text-sm bg-secondaryAlt  rounded-md ">Honey</button>
-            </div>
-            <p class="text-textPrimary">Connect..</p>
-                <div class="p-1 grid grid-cols-2 h-[40vh] gap-2">
-                    <button class="border border-secondary  px-3 row-span-4   py-2 text-secondary text-sm bg-secondaryAlt rounded-md">what you want</button>
-                    <button class="border border-secondary  px-3 row-span-4  py-2 text-secondary text-sm bg-secondaryAlt rounded-md">what you have</button>
+            <div class="space-y-2">
+                <p class="text-textPrimary text-sm  flex justify-between" ><span>connect...</span> <span class="text-secondary text-xs">*</span></p>
+                <div class="p-1 grid grid-cols-2 gap-3">
+                    <button class="borde   px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">what you what</button>
+                    <button class="borde px-3 py-2   text-secondary text-sm bg-secondaryAlt  rounded-md ">what you what</button>
                 </div>
-
-            <p class="text-textPrimary">Connect..</p>
-            <div class="p-1 flex justify-around gap-x-3">
-                <button class="borde px-3 flex-1  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">what you what</button>
-                <button class="borde px-3 py-2 flex-1  text-secondary text-sm bg-secondaryAlt  rounded-md ">what you what</button>
+            </div>
+            <div class="space-y-2">
+                <p class="text-textPrimary text-sm  flex justify-between" ><span>film-making</span> <span class="text-secondary text-xs">*</span></p>
+                <div class="p-1 grid grid-cols-2 gap-3">
+                    <button class="borde   px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">pre-production</button>
+                    <button class="borde px-3 py-2   text-secondary text-sm bg-secondaryAlt  rounded-md ">post-production</button>
+                    <button class="borde px-3 py-2 col-span-2   text-secondary text-sm bg-secondaryAlt  rounded-md ">production</button>
+                </div>
+            </div>
+            <div class="space-y-2">
+                <p class="text-textPrimary text-sm  flex justify-between" ><span>markting</span> <span class="text-secondary text-xs">*</span></p>
+                <div class="p-1 grid grid-cols-2 gap-3">
+                    <button class="borde   px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">Distribution</button>
+                    <button class="borde px-3 py-2   text-secondary text-sm bg-secondaryAlt  rounded-md ">Promotions</button>
+                    <button class="borde px-3 py-2   text-secondary text-sm bg-secondaryAlt  rounded-md ">Premiere</button>
+                    <button class="borde px-3 py-2   text-secondary text-sm bg-secondaryAlt  rounded-md ">& Release</button>
+                </div>
+            </div>
+            <div class="space-y-2">
+                <p class="text-textPrimary text-sm  flex justify-between" ><span>short-film</span> <span class="text-secondary text-xs">*</span></p>
+                <div class="p-1 grid grid-cols-2 gap-3 h-[35vh]">
+                    <button class="borde row-span-3  flex justify-start items-end     px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">image</button>
+                    <button class="borde row-span-1  px-3 py-2   text-secondary text-sm bg-secondaryAlt  rounded-md ">profile</button>
+                    <button class="borde row-span-1  px-3 py-2   text-secondary text-sm bg-secondaryAlt  rounded-md ">what you what</button>
+                    <button class="borde row-span-1  px-3 py-2   text-secondary text-sm bg-secondaryAlt  rounded-md ">what you what</button>
+                </div>
+            </div>
+            <div class="space-y-2  ">
+                <p class="text-textPrimary text-sm  flex justify-between" ><span>latest-updates</span> <span class="text-secondary text-xs">*</span></p>
+                <div class="p-1 grid grid-cols-2 gap-3">
+                    <button class="borde   col-span-2  px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">what you what</button>
+                    <button class="borde px-3 py-2   text-secondary text-sm bg-secondaryAlt  rounded-md ">what you what</button>
+                </div>
+            </div>
+            <div class="space-y-2  ">
+                <p class="text-textPrimary text-sm  flex justify-between" ><span>equipments</span> <span class="text-secondary text-xs">*</span></p>
+                <div class="p-1 grid grid-cols-3 gap-3 h-[28vh]">
+                    <button class="borde flex justify-center items-end  row-span-2  px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">tripod</button>
+                    <button class="borde   row-span-2  px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">camera</button>
+                    <button class="borde   row-span-2  px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">gimble</button>
+                    <button class="borde   row-span-2  px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">drone</button>
+                    <button class="borde   row-span-2  px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">lens</button>
+                    <button class="borde   row-span-2  px-3  py-2 text-secondary text-sm bg-secondaryAlt  rounded-md ">filters</button>
+                 
+                </div>
             </div>
         </section>
         <section class="box-2 borderr-4 borderr-red-400">box2</section>
